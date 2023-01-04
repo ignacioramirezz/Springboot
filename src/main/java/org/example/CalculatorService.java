9273ee266bf17cdc58c1272e8def1680a21683b2
ignacioramirezz <82910997+ignacioramirezz@users.noreply.github.com>
2023-01-04 15:33:46 -0300

    primer commit

---
+++ src/main/java/org/example/CalculatorService.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class CalculatorService {
+
+    public CalculatorService(){
+        System.out.println("ejecutando el constructor");
+    }
+
+    public String holaMundo(){
+        return "Hola mundo";
+    }
+
+
+}
