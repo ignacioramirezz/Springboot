f47c8c17b55ffb191084ba774d6345c44010c465
ignacioramirezz <82910997+ignacioramirezz@users.noreply.github.com>
2023-01-10 10:31:10 -0300

    spring data jpa

---
+++ rest-datajpa/rest-datajpa/src/main/java/com/example/restdatajpa/RestDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDatajpaApplication.class, args);
+	}
+
+}
---
+++ rest-datajpa/rest-datajpa/src/test/java/com/example/restdatajpa/RestDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
